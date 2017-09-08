1c4fca3f0410d201d01734e51fa2b4fdcdb93917
YiJiangFengYun <yijiangfengyun@outlook.com>
2017-09-08 20:03:29 +0800

    Add Material data members and their getset method.

--- framework/include/graphics/material/material.hpp
+++ framework/include/graphics/material/material.hpp
@@ -18,6 +18,8 @@ namespace kgs
 
 		int32_t getPassIndex(std::string name);
 		std::string getPassName(int32_t index);
+		std::shared_ptr<Pass> getPass(std::string name);
+		void setPass(std::string name, std::shared_ptr<Pass> pass);
 
 		Color getColor(std::string name);
 		void setColor(std::string name, Color value);
@@ -32,7 +34,7 @@ namespace kgs
 		int32_t getInt(std::string name);
 		void setInt(std::string name, int32_t value);
 		std::vector<int32_t> getIntArray(std::string name);
-		void setIntArray(std::string anme, std::vector<int32_t> values);
+		void setIntArray(std::string name, std::vector<int32_t> values);
 
 		Vector4 getVector(std::string name);
 		void setVector(std::string name, Vector4 value);
@@ -53,20 +55,81 @@ namespace kgs
 
 	private:
 		//--compositions
+		uint32_t m_renderQueue;
 		Color m_mainColor;
 		Vector2 m_mainTextureOffset;
 		Vector2 m_mainTextureScale;
-		uint32_t m_renderQueue;
 		//--compositions
 
 		//--aggregations
 		std::vector<std::shared_ptr<Pass>> m_arrPasses;
 		std::unordered_map<std::string, std::shared_ptr<Pass>> m_mapPasses;
 
+		std::vector<Color> m_arrColors;
+		std::unordered_map<std::string, Color> m_mapColors;
+		std::vector<std::vector<Color>> m_arrColorArrays;
+		std::unordered_map<std::string, std::vector<Color>> m_mapColorArrays;
+
+		std::vector<float> m_arrFloats;
+		std::unordered_map<std::string, float> m_mapFloats;
+		std::vector<std::vector<float>> m_arrFloatArrays;
+		std::unordered_map<std::string, std::vector<float>> m_mapFloatArrays;
+
+		std::vector<int32_t> m_arrInts;
+		std::unordered_map<std::string, int32_t> m_mapInts;
+		std::vector<std::vector<int32_t>> m_arrIntArrays;
+		std::unordered_map<std::string, std::vector<int32_t>> m_mapIntArrays;
+
+		std::vector<Vector4> m_arrVectors;
+		std::unordered_map<std::string, Vector4> m_mapVectors;
+		std::vector<std::vector<Vector4>> m_arrVectorArrays;
+		std::unordered_map<std::string, std::vector<Vector4>> m_mapVectorArrays;
+
+		std::vector<Matrix4x4> m_arrMatrixs;
+		std::unordered_map<std::string, Matrix4x4> m_mapMatrixs;
+		std::vector<std::vector<Matrix4x4>> m_arrMatrixArrays;
+		std::unordered_map<std::string, std::vector<Matrix4x4>> m_mapMatrixArrays;
+
 		std::shared_ptr<Texture> m_mainTexture;
 		std::vector<std::shared_ptr<Texture>> m_arrTextures;
 		std::unordered_map<std::string, std::shared_ptr<Texture>> m_mapTextures;
+		std::vector<Vector2> m_arrTextureOffsets;
+		std::unordered_map<std::string, Vector2> m_mapTextureOffsets;
+		std::vector<Vector2> m_arrTextureScales;
+		std::unordered_map<std::string, Vector2> m_mapTextureScales;
 		//--aggregations
+
+		//tool methods
+		template <typename T>
+		inline T getValue(std::string key, std::unordered_map<std::string, T> map, std::vector<T> arr)
+		{
+			auto iterator = map.find(name);
+			if (iterator == map.end())
+			{
+				return {};
+			}
+			else
+			{
+				return iterator->second;
+			}
+		}
+
+		template <typename T>
+		inline void setValue(std::string key, T value, std::unordered_map<std::string, T> map, std::vector<T> arr)
+		{
+			auto iterator = map.find(name);
+			if (iterator == map.end())
+			{
+				map.insert(std::make_pair(name, value));
+				arr.push_back(value);
+			}
+			else
+			{
+				iterator->second = value;
+				auto iterator2 = std::find(arr.begin(), arr.end(), iterator->second);
+				*iterator2 = value;
+			}
+		}
 	};
 }
 
--- framework/src/graphics/material/material.cpp
+++ framework/src/graphics/material/material.cpp
@@ -9,4 +9,164 @@ namespace kgs
 	Material::~Material()
 	{
 	}
+
+	int32_t Material::getPassIndex(std::string name)
+	{
+		auto iterator = m_mapPasses.find(name);
+		if (iterator == m_mapPasses.end()) return -1;
+		//linear complexity
+		auto iterator2 = std::find(m_arrPasses.cbegin(), m_arrPasses.cend(), iterator->second);
+		return static_cast<int32_t>(iterator2 - m_arrPasses.cbegin());
+
+	}
+
+	std::string Material::getPassName(int32_t index)
+	{
+		auto pass = *(m_arrPasses.cbegin() + index);
+		//linear complexity
+		auto pos = std::find_if(m_mapPasses.cbegin(), m_mapPasses.cend(), [pass](const std::pair<std::string, std::shared_ptr<Pass>>& elem) {
+			return elem.second == pass;
+		});
+		return pos->first;
+	}
+
+	std::shared_ptr<Pass> Material::getPass(std::string name)
+	{
+		return getValue(name, m_mapPasses, m_arrPasses);
+	}
+
+	void Material::setPass(std::string name, std::shared_ptr<Pass> pass)
+	{
+		setValue(name, pass, m_mapPasses, m_arrPasses);
+	}
+
+	Color Material::getColor(std::string name)
+	{
+		return getValue(name, m_mapColors, m_arrColors);
+	}
+
+	void Material::setColor(std::string name, Color value)
+	{
+		setValue(name, value, m_mapColors, m_arrColors);
+	}
+
+	std::vector<Color> Material::getColorArray(std::string name)
+	{
+		return getValue(name, m_mapColorArrays, m_arrColorArrays);
+	}
+
+	void Material::setColorArray(std::string name, std::vector<Color> values)
+	{
+		setValue(name, values, m_mapColorArrays, m_arrColorArrays);
+	}
+
+	float Material::getFloat(std::string name)
+	{
+		return getValue(name, m_mapFloats, m_arrFloats);
+	}
+
+	void Material::setFloat(std::string name, float value)
+	{
+		setValue(name, value, m_mapFloats, m_arrFloats);
+	}
+
+	std::vector<float> Material::getFloatArray(std::string name)
+	{
+		return getValue(name, m_mapFloatArrays, m_arrFloatArrays);
+	}
+
+	void Material::setFloatArray(std::string name, std::vector<float> values)
+	{
+		setValue(name, values, m_mapFloatArrays, m_arrFloatArrays);
+	}
+
+	int32_t Material::getInt(std::string name)
+	{
+		return getValue(name, m_mapInts, m_arrInts);
+	}
+
+	void Material::setInt(std::string name, int32_t value)
+	{
+		setValue(name, value, m_mapInts, m_arrInts);
+	}
+
+	std::vector<int32_t> Material::getIntArray(std::string name)
+	{
+		return getValue(name, m_mapIntArrays, m_arrIntArrays);
+	}
+
+	void Material::setIntArray(std::string name, std::vector<int32_t> values)
+	{
+		setValue(name, values, m_mapIntArrays, m_arrIntArrays);
+	}
+
+	Vector4 Material::getVector(std::string name)
+	{
+		return getValue(name, m_mapVectors, m_arrVectors);
+	}
+
+	void Material::setVector(std::string name, Vector4 value)
+	{
+		setValue(name, value, m_mapVectors, m_arrVectors);
+	}
+
+	std::vector<Vector4> Material::getVectorArray(std::string name)
+	{
+		return getValue(name, m_mapVectorArrays, m_arrVectorArrays);
+	}
+
+	void Material::setVectorArray(std::string name, std::vector<Vector4> values)
+	{
+		setValue(name, values, m_mapVectorArrays, m_arrVectorArrays);
+	}
+
+	Matrix4x4 Material::getMatrix(std::string name)
+	{
+		return getValue(name, m_mapMatrixs, m_arrMatrixs);
+	}
+
+	void Material::setMatrix(std::string name, Matrix4x4 value)
+	{
+		setValue(name, value, m_mapMatrixs, m_arrMatrixs);
+	}
+
+	std::vector<Matrix4x4> Material::getMatrixArray(std::string name)
+	{
+		return getValue(name, m_mapMatrixArrays, m_arrMatrixArrays);
+	}
+
+	void Material::setMatrixArray(std::string name, std::vector<Matrix4x4> values)
+	{
+		setValue(name, values, m_mapMatrixArrays, m_arrMatrixArrays);
+	}
+
+	std::shared_ptr<Texture> Material::getTexture(std::string name)
+	{
+		return getValue(name, m_mapTextures, m_arrTextures);
+	}
+
+	void Material::setTexture(std::string name, std::shared_ptr<Texture> value)
+	{
+		setValue(name, value, m_mapTextures, m_arrTextures);
+	}
+
+	Vector2 Material::getTextureOffset(std::string name)
+	{
+		return getValue(name, m_mapTextureOffsets, m_arrTextureOffsets);
+	}
+
+	void Material::setTextureOffset(std::string name, Vector2 value)
+	{
+		setValue(name, value, m_mapTextureOffsets, m_arrTextureOffsets);
+	}
+
+	Vector2 Material::getTextureScale(std::string name)
+	{
+		return getValue(name, m_mapTextureScales, m_arrTextureScales);
+	}
+
+	void Material::setTextureScale(std::string name, Vector2 value)
+	{
+		setValue(name, value, m_mapTextureScales, m_arrTextureScales);
+	}
 }

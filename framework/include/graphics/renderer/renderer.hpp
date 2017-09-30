46b76318b1ca886407413c0bac20f57496d884b7
YiJiangFengYun <yijiangfengyun@outlook.com>
2017-09-30 10:20:30 +0800

    Add base implements of Scene and Renderer.

--- framework/include/graphics/renderer/renderer.hpp
+++ framework/include/graphics/renderer/renderer.hpp
@@ -1,9 +1,41 @@
 #ifndef KGS_RENDERER_H
 #define KGS_RENDERER_H
 
+#include "graphics/scene/option.hpp"
+#include "graphics/scene/scene.hpp"
+#include "graphics/scene/camera.hpp"
+
 namespace kgs
 {
+	template <SpaceType SPACE_TYPE>
+	class Renderer
+	{
+	public:
+		typedef Scene<SPACE_TYPE> SceneType;
+		typedef Camera<SPACE_TYPE> CameraType;
 
+		Renderer()
+		{
+
+		}
+
+		Renderer(std::shared_ptr<SceneType> pScene, std::shared_ptr<CameraType> pCamera)
+			: m_PScene(pScene)
+			, m_pCamera(pCamera)
+		{
+
+		}
+
+		render()
+		{
+
+		}
+	protected:
+		//aggregations
+		std::shared_ptr<SceneType> m_pScene;
+		std::shared_ptr<CameraType> m_pCamera;
+
+	};
 } //namespace kgs
 
 #endif // !KGS_RENDERER_H
--- framework/include/graphics/scene/scene.hpp
+++ framework/include/graphics/scene/scene.hpp
@@ -1,9 +1,96 @@
 #ifndef KGS_SCENE_H
 #define KGS_SCENE_H
 
+#include <vector>
+#include <unordered_map>
+#include "graphics/util/util.hpp"
+#include "graphics/scene/object.hpp"
+#include "graphics/scene/visual_object.hpp"
+#include "graphics/scene/camera.hpp"
+#include "graphics/scene/light.hpp"
+
 namespace kgs
 {
+	template <SpaceType SPACE_TYPE>
+	class Scene
+	{
+	public:
+		typedef Object<SPACE_TYPE> ObjectType;
+		typedef VisualObject<SPACE_TYPE> VisualObject;
+		typedef Camera<SPACE_TYPE> CameraType;
+		typedef Light<SPACE_TYPE> LightType;
 
+		uint32_t getVisualObjectCount()
+		{
+			return m_arrPVisualObjects.size();
+		}
+
+		std::shared_ptr<VisualObject> getVisualObjectWithIndex(uint32_t index)
+		{
+			return m_arrPVisualObjects[index];
+		}
+
+		std::shared_ptr<VisualObject> getVisualObjectWithName(std::string name)
+		{
+			getValue(name, m_mapPVisualObjects, m_arrPVisualObjects);
+		}
+
+		void setVisualObjectWithName(std::string name, std::shared_ptr<VisualObject> value)
+		{
+			setValue(name, value, m_mapPVisualObjects, m_arrPVisualObjects);
+		}
+
+		uint32_t getCameraCount()
+		{
+			return m_arrPCameras.size();
+		}
+
+		std::shared_ptr<CameraType> getCameraWithIndex(uint32_t index)
+		{
+			return m_arrPCameras[index];
+		}
+
+		std::shared_ptr<CameraType> getCameraWithName(std::string name)
+		{
+			getValue(name, m_mapPCameras, m_arrPCameras);
+		}
+
+		void setCameraWithName(std::string name, std::shared_ptr<CameraType> value)
+		{
+			setValue(name, value, m_mapPCameras, m_arrPCameras);
+		}
+
+		uint32_t getLightCount()
+		{
+			return m_arrPLights.size();
+		}
+
+		std::shared_ptr<LightType> getLightWithIndex(uint32_t index)
+		{
+			return m_arrPLights[index];
+		}
+
+		std::shared_ptr<LightType> getLightWithName(std::string name)
+		{
+			getValue(name, m_mapPLights, m_arrPLights);
+		}
+
+		void setLightWithName(std::string name, std::shared_ptr<LightType> value)
+		{
+			setValue(name, value, m_mapPLights, m_arrPLights);
+		}
+
+	protected:
+
+		//aggregations
+		std::vector<std::shared_ptr<VisualObject>> m_arrPVisualObjects;
+		std::unordered_map<std::string, std::shared_ptr<VisualObject>> m_mapPVisualObjects;
+		std::vector<std::shared_ptr<CameraType>> m_arrPCameras;
+		std::unordered_map<std::string, std::shared_ptr<CameraType>> m_mapPCameras;
+		std::vector<std::shared_ptr<LightType>> m_arrPLights;
+		std::unordered_map<std::string, std::shared_ptr<LightType>> m_mapPLights;
+
+	};
 } //namespace kgs
 
 #endif // !KGS_SCENE_H

b2e28254698d1879af23c8ce27cd82e8a780f2b5
YiJiangFengYun <yijiangfengyun@outlook.com>
2017-10-28 08:59:52 +0800

    Change cmd of post build event of vc to compile shaders and copy resources for test.

--- test/include/test-triangle/window.hpp
+++ test/include/test-triangle/window.hpp
@@ -149,8 +149,8 @@ namespace testTriangle
 
 namespace std {
 	template<> 
-	struct hash<app::Window::Vertex> {
-		size_t operator()(const app::Window::Vertex& vertex) const {
+	struct hash<testTriangle::Window::Vertex> {
+		size_t operator()(const testTriangle::Window::Vertex& vertex) const {
 			return ((hash<glm::vec3>()(vertex.pos) ^
 				(hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^
 				(hash<glm::vec2>()(vertex.texCoord) << 1);

3cf0a647e33aa43374c1d2a0ce7169941b070bf1
YiJiangFengYun <yijiangfengyun@outlook.com>
2018-02-01 20:32:29 +0800

    Add new code files. Include: renderer_2.hpp, renderer_3.hpp and renderer_indep.hpp.

---
+++ framework/vgim/renderer_2.hpp
@@ -0,0 +1,9 @@
+#ifndef VG_IM_RENDERER_2_HPP
+#define VG_IM_RENDERER_2_HPP
+
+namespace vgim
+{
+    
+}
+
+#endif //VG_IM_RENDERER_2_HPP
---
+++ framework/vgim/renderer_3.hpp
@@ -0,0 +1,9 @@
+#ifndef VG_IM_RENDERER_3_HPP
+#define VG_IM_RENDERER_3_HPP
+
+namespace vgim
+{
+
+}
+
+#endif //VG_IM_RENDERER_3_HPP

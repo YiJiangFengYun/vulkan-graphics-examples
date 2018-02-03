ee087f892d8829817c17d2ddd337862d47cbfe1e
YiJiangFengYun <yijiangfengyun@outlook.com>
2018-02-03 18:28:06 +0800

    vgim developing.

--- framework/vgim/module.cpp
+++ framework/vgim/module.cpp
@@ -1,9 +1,95 @@
 #include "vgim/module.hpp"
 
 namespace vgim {
-	void static moduleCreate()
+	void _createMaterial();
+    void _createMesh();
+    
+    DimType m_dimType;
+	static const uint32_t __glsl_shader_vert_spv[] =
+    {
+        0x07230203,0x00010000,0x00080001,0x0000002e,0x00000000,0x00020011,0x00000001,0x0006000b,
+        0x00000001,0x4c534c47,0x6474732e,0x3035342e,0x00000000,0x0003000e,0x00000000,0x00000001,
+        0x000a000f,0x00000000,0x00000004,0x6e69616d,0x00000000,0x0000000b,0x0000000f,0x00000015,
+        0x0000001b,0x0000001c,0x00030003,0x00000002,0x000001c2,0x00040005,0x00000004,0x6e69616d,
+        0x00000000,0x00030005,0x00000009,0x00000000,0x00050006,0x00000009,0x00000000,0x6f6c6f43,
+        0x00000072,0x00040006,0x00000009,0x00000001,0x00005655,0x00030005,0x0000000b,0x0074754f,
+        0x00040005,0x0000000f,0x6c6f4361,0x0000726f,0x00030005,0x00000015,0x00565561,0x00060005,
+        0x00000019,0x505f6c67,0x65567265,0x78657472,0x00000000,0x00060006,0x00000019,0x00000000,
+        0x505f6c67,0x7469736f,0x006e6f69,0x00030005,0x0000001b,0x00000000,0x00040005,0x0000001c,
+        0x736f5061,0x00000000,0x00060005,0x0000001e,0x73755075,0x6e6f4368,0x6e617473,0x00000074,
+        0x00050006,0x0000001e,0x00000000,0x61635375,0x0000656c,0x00060006,0x0000001e,0x00000001,
+        0x61725475,0x616c736e,0x00006574,0x00030005,0x00000020,0x00006370,0x00040047,0x0000000b,
+        0x0000001e,0x00000000,0x00040047,0x0000000f,0x0000001e,0x00000002,0x00040047,0x00000015,
+        0x0000001e,0x00000001,0x00050048,0x00000019,0x00000000,0x0000000b,0x00000000,0x00030047,
+        0x00000019,0x00000002,0x00040047,0x0000001c,0x0000001e,0x00000000,0x00050048,0x0000001e,
+        0x00000000,0x00000023,0x00000000,0x00050048,0x0000001e,0x00000001,0x00000023,0x00000008,
+        0x00030047,0x0000001e,0x00000002,0x00020013,0x00000002,0x00030021,0x00000003,0x00000002,
+        0x00030016,0x00000006,0x00000020,0x00040017,0x00000007,0x00000006,0x00000004,0x00040017,
+        0x00000008,0x00000006,0x00000002,0x0004001e,0x00000009,0x00000007,0x00000008,0x00040020,
+        0x0000000a,0x00000003,0x00000009,0x0004003b,0x0000000a,0x0000000b,0x00000003,0x00040015,
+        0x0000000c,0x00000020,0x00000001,0x0004002b,0x0000000c,0x0000000d,0x00000000,0x00040020,
+        0x0000000e,0x00000001,0x00000007,0x0004003b,0x0000000e,0x0000000f,0x00000001,0x00040020,
+        0x00000011,0x00000003,0x00000007,0x0004002b,0x0000000c,0x00000013,0x00000001,0x00040020,
+        0x00000014,0x00000001,0x00000008,0x0004003b,0x00000014,0x00000015,0x00000001,0x00040020,
+        0x00000017,0x00000003,0x00000008,0x0003001e,0x00000019,0x00000007,0x00040020,0x0000001a,
+        0x00000003,0x00000019,0x0004003b,0x0000001a,0x0000001b,0x00000003,0x0004003b,0x00000014,
+        0x0000001c,0x00000001,0x0004001e,0x0000001e,0x00000008,0x00000008,0x00040020,0x0000001f,
+        0x00000009,0x0000001e,0x0004003b,0x0000001f,0x00000020,0x00000009,0x00040020,0x00000021,
+        0x00000009,0x00000008,0x0004002b,0x00000006,0x00000028,0x00000000,0x0004002b,0x00000006,
+        0x00000029,0x3f800000,0x00050036,0x00000002,0x00000004,0x00000000,0x00000003,0x000200f8,
+        0x00000005,0x0004003d,0x00000007,0x00000010,0x0000000f,0x00050041,0x00000011,0x00000012,
+        0x0000000b,0x0000000d,0x0003003e,0x00000012,0x00000010,0x0004003d,0x00000008,0x00000016,
+        0x00000015,0x00050041,0x00000017,0x00000018,0x0000000b,0x00000013,0x0003003e,0x00000018,
+        0x00000016,0x0004003d,0x00000008,0x0000001d,0x0000001c,0x00050041,0x00000021,0x00000022,
+        0x00000020,0x0000000d,0x0004003d,0x00000008,0x00000023,0x00000022,0x00050085,0x00000008,
+        0x00000024,0x0000001d,0x00000023,0x00050041,0x00000021,0x00000025,0x00000020,0x00000013,
+        0x0004003d,0x00000008,0x00000026,0x00000025,0x00050081,0x00000008,0x00000027,0x00000024,
+        0x00000026,0x00050051,0x00000006,0x0000002a,0x00000027,0x00000000,0x00050051,0x00000006,
+        0x0000002b,0x00000027,0x00000001,0x00070050,0x00000007,0x0000002c,0x0000002a,0x0000002b,
+        0x00000028,0x00000029,0x00050041,0x00000011,0x0000002d,0x0000001b,0x0000000d,0x0003003e,
+        0x0000002d,0x0000002c,0x000100fd,0x00010038
+    };
+    
+    static const uint32_t __glsl_shader_frag_spv[] =
+    {
+        0x07230203,0x00010000,0x00080001,0x0000001e,0x00000000,0x00020011,0x00000001,0x0006000b,
+        0x00000001,0x4c534c47,0x6474732e,0x3035342e,0x00000000,0x0003000e,0x00000000,0x00000001,
+        0x0007000f,0x00000004,0x00000004,0x6e69616d,0x00000000,0x00000009,0x0000000d,0x00030010,
+        0x00000004,0x00000007,0x00030003,0x00000002,0x000001c2,0x00040005,0x00000004,0x6e69616d,
+        0x00000000,0x00040005,0x00000009,0x6c6f4366,0x0000726f,0x00030005,0x0000000b,0x00000000,
+        0x00050006,0x0000000b,0x00000000,0x6f6c6f43,0x00000072,0x00040006,0x0000000b,0x00000001,
+        0x00005655,0x00030005,0x0000000d,0x00006e49,0x00050005,0x00000016,0x78655473,0x65727574,
+        0x00000000,0x00040047,0x00000009,0x0000001e,0x00000000,0x00040047,0x0000000d,0x0000001e,
+        0x00000000,0x00040047,0x00000016,0x00000022,0x00000000,0x00040047,0x00000016,0x00000021,
+        0x00000000,0x00020013,0x00000002,0x00030021,0x00000003,0x00000002,0x00030016,0x00000006,
+        0x00000020,0x00040017,0x00000007,0x00000006,0x00000004,0x00040020,0x00000008,0x00000003,
+        0x00000007,0x0004003b,0x00000008,0x00000009,0x00000003,0x00040017,0x0000000a,0x00000006,
+        0x00000002,0x0004001e,0x0000000b,0x00000007,0x0000000a,0x00040020,0x0000000c,0x00000001,
+        0x0000000b,0x0004003b,0x0000000c,0x0000000d,0x00000001,0x00040015,0x0000000e,0x00000020,
+        0x00000001,0x0004002b,0x0000000e,0x0000000f,0x00000000,0x00040020,0x00000010,0x00000001,
+        0x00000007,0x00090019,0x00000013,0x00000006,0x00000001,0x00000000,0x00000000,0x00000000,
+        0x00000001,0x00000000,0x0003001b,0x00000014,0x00000013,0x00040020,0x00000015,0x00000000,
+        0x00000014,0x0004003b,0x00000015,0x00000016,0x00000000,0x0004002b,0x0000000e,0x00000018,
+        0x00000001,0x00040020,0x00000019,0x00000001,0x0000000a,0x00050036,0x00000002,0x00000004,
+        0x00000000,0x00000003,0x000200f8,0x00000005,0x00050041,0x00000010,0x00000011,0x0000000d,
+        0x0000000f,0x0004003d,0x00000007,0x00000012,0x00000011,0x0004003d,0x00000014,0x00000017,
+        0x00000016,0x00050041,0x00000019,0x0000001a,0x0000000d,0x00000018,0x0004003d,0x0000000a,
+        0x0000001b,0x0000001a,0x00050057,0x00000007,0x0000001c,0x00000017,0x0000001b,0x00050085,
+        0x00000007,0x0000001d,0x00000012,0x0000001c,0x0003003e,0x00000009,0x0000001d,0x000100fd,
+        0x00010038
+    };
+	
+    std::shared_ptr<vg::Shader> m_pShader;
+    std::shared_ptr<vg::Pass> m_pPass;
+    std::shared_ptr<vg::Material> m_pMaterial;
+    
+    std::shared_ptr<vg::SimpleMesh> m_pMesh;
+
+	void static moduleCreate(DimType dimensionType)
 	{
 		if (isInited == VG_TRUE) return;
+        m_dimType = dimensionType;
+		_createMaterial();
 		//Indicate module was initialized.
 		isInited = VG_IM_TRUE;
 	}
@@ -14,9 +100,92 @@ namespace vgim {
 		isInited = VG_IM_FALSE;
 	}
 
-    void renderImGUI()
+	const std::shared_ptr<vg::Material> getMaterial()
+	{
+		return m_pMaterial;
+	}
+
+    const std::shared_ptr<vg::SimpleMesh> getMesh()
+    {
+        return m_pMesh;   
+    }
+
+    void updateFromImGUI()
     {
         auto drawData = ImGui::GetDrawData();
-		
+		uint32_t vertexCount = drawData->TotalVtxCount;
+        uint32_t indexCount = drawData->TotalIdxCount;
+        uint32_t vertexSize = static_cast<uint32_t>(vertexCount * sizeof(ImDrawVert));
+        uint32_t indexSize = static_cast<uint32_t>(indexCount * sizeof(ImDrawIdx));
+
+        const auto& pVertexData = m_pMesh->getVertexData();
+        const auto& pIndexData = m_pMesh->getIndexData();
+
+        pVertexData->updateVertexCount(vertexCount);
+        pVertexData->updateBufferSize(vertexSize);
+
+        pIndexData->updateIndexCount(indexCount);
+        pIndexData->updateBufferSize(indexSize);
+        
     }
+
+	void _createMaterial()
+	{
+        m_pShader = std::shared_ptr<vg::Shader>(new vg::Shader(__glsl_shader_vert_spv, 
+            static_cast<uint32_t>(sizeof(__glsl_shader_vert_spv) / sizeof(uint32_t)),
+            __glsl_shader_frag_spv,
+            static_cast<uint32_t>(sizeof(__glsl_shader_frag_spv) / sizeof(uint32_t))));
+		m_pPass = std::shared_ptr<vg::Pass>(new vg::Pass(m_pShader));
+        m_pPass->setPushConstantRange("default", vk::ShaderStageFlagBits::eVertex, static_cast<uint32_t>(sizeof(float) * 0), static_cast<uint32_t>(sizeof(float) * 4));
+		m_pMaterial = std::shared_ptr<vg::Material>(new vg::Material());
+		m_pMaterial->addPass(m_pPass);
+		m_pMaterial->setRenderPriority(0u);
+		m_pMaterial->setRenderQueueType(vg::MaterialShowType::TRANSPARENT);
+		m_pMaterial->apply();
+	}
+
+     void _createMesh()
+     {
+        if (m_dimType == DimType::SPACE_2) 
+        {
+           m_pMesh = static_cast<std::shared_ptr<vg::SimpleMesh>>(new vg::DimSimpleMesh2());
+        }
+        else
+        {
+           m_pMesh = static_cast<std::shared_ptr<vg::SimpleMesh>>(new vg::DimSimpleMesh3());            
+        }
+
+        const auto& pVertexData = m_pMesh->getVertexData();
+        const auto& pIndexData = m_pMesh->getIndexData();
+
+        vk::VertexInputBindingDescription bindingDesc[1] = {};
+        bindingDesc[0].stride = sizeof(ImDrawVert);
+        bindingDesc[0].inputRate = vk::VertexInputRate::eVertex;
+    
+        vk::VertexInputAttributeDescription attributeDesc[3] = {};
+        attributeDesc[0].location = 0;
+        attributeDesc[0].binding = bindingDesc[0].binding;
+        attributeDesc[0].format = vk::Format::eR32G32Sfloat;
+        attributeDesc[0].offset = (size_t)(&((ImDrawVert*)0)->pos);
+        attributeDesc[1].location = 1;
+        attributeDesc[1].binding = bindingDesc[0].binding;
+        attributeDesc[1].format = vk::Format::eR32G32Sfloat;
+        attributeDesc[1].offset = (size_t)(&((ImDrawVert*)0)->uv);
+        attributeDesc[2].location = 2;
+        attributeDesc[2].binding = bindingDesc[0].binding;
+        attributeDesc[2].format = vk::Format::eR8G8B8A8Unorm;
+        attributeDesc[2].offset = (size_t)(&((ImDrawVert*)0)->col);
+    
+        vk::PipelineVertexInputStateCreateInfo vertexInfo = {};
+        vertexInfo.vertexBindingDescriptionCount = 1;
+        vertexInfo.pVertexBindingDescriptions = bindingDesc;
+        vertexInfo.vertexAttributeDescriptionCount = 3;
+        vertexInfo.pVertexAttributeDescriptions = attributeDesc;
+    
+        vk::PipelineInputAssemblyStateCreateInfo iaInfo = {};
+        iaInfo.topology = vk::PrimitiveTopology::eTriangleList;
+
+        pVertexData->updateDesData(vertexInfo);
+        pIndexData->updateDesData(iaInfo);
+     }
 } //vgim
--- framework/vgim/module.hpp
+++ framework/vgim/module.hpp
@@ -4,9 +4,20 @@
 #include "vgim/global.hpp"
 
 namespace vgim {
+	enum class DimType
+	{
+		SPACE_2 = static_cast<int32_t>(vg::MeshDimType::SPACE_2),
+		SPACE_3 = static_cast<int32_t>(vg::MeshDimType::SPACE_3),
+		BEGIN_RANGE = SPACE_2,
+		END_RANGE = SPACE_3,
+		RANGE_SIZE = (END_RANGE - BEGIN_RANGE + 1)
+	};
+
 	extern Bool32 isInited;
-	extern void moduleCreate();
+	extern void moduleCreate(DimType dimensionType);
 	extern void moduleDestory();
-	extern void renderImGUI();
+	extern const std::shared_ptr<vg::Material> getMaterial();
+	extern const std::shared_ptr<vg::SimpleMesh> getMesh();
+	extern void updateFromImGUI();
 } //vgim
 #endif //VG_IM_MODULE_H

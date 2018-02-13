e1996fcbcb7bf178c335d1b73d8a54676caaaf62
YiJiangFengYun <yijiangfengyun@outlook.com>
2018-02-13 08:08:05 +0800

    Delete useless files of vgim module.

--- framework/vgim/renderer_2.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef VG_IM_RENDERER_2_HPP
-#define VG_IM_RENDERER_2_HPP
-
-namespace vgim
-{
-    
-}
-
-#endif //VG_IM_RENDERER_2_HPP
--- framework/vgim/renderer_3.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef VG_IM_RENDERER_3_HPP
-#define VG_IM_RENDERER_3_HPP
-
-namespace vgim
-{
-
-}
-
-#endif //VG_IM_RENDERER_3_HPP

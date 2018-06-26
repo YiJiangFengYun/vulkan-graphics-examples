4d3652f5890ca01205332dacc699db3d2e1478e5
YiJiangFengYun <yijiangfengyun@outlook.com>
2018-06-26 19:32:27 +0800

    Add class TextureCubeColorAttachment, TextureCubeDepthStencilAttachment and TextureCubeDepthAttachment.

--- framework/graphics/graphics/texture/texture_cube.cpp
+++ framework/graphics/graphics/texture/texture_cube.cpp
@@ -48,4 +48,301 @@ namespace vg
     {
         _applyData(layoutInfo, memory, size, cacheMemory, createMipmaps);
     }
+
+    TextureCubeColorAttachment::TextureCubeColorAttachment(vk::Format format
+        , uint32_t width
+        , uint32_t height
+        , Bool32 isInputUsage
+        , vk::ImageUsageFlags additionalUsage
+        , Bool32 defaultImageView
+        , Bool32 defaultSampler
+        )
+        : Texture(format
+        , false
+        , additionalUsage
+        , defaultImageView
+        , defaultSampler
+        )
+        , BaseColorAttachment(isInputUsage)
+    {
+        m_type = TextureType::CUBE_COLOR_ATTACHMENT;
+        m_width = width;
+        m_height = height;
+        m_allAspectFlags = vk::ImageAspectFlagBits::eColor;
+        m_usageFlags = vk::ImageUsageFlagBits::eSampled | vk::ImageUsageFlagBits::eColorAttachment;
+        m_layout = vk::ImageLayout::eShaderReadOnlyOptimal;
+        if (isInputUsage)
+        {
+            m_usageFlags |= vk::ImageUsageFlagBits::eInputAttachment;
+        }
+        _init(VG_FALSE);
+    }
+
+    TextureCubeColorAttachment::~TextureCubeColorAttachment()
+    {
+    }
+
+    uint32_t TextureCubeColorAttachment::getWidth() const
+    {
+        return m_width;
+    }
+
+    uint32_t TextureCubeColorAttachment::getHeight() const
+    {
+        return m_height;
+    }
+
+    void TextureCubeColorAttachment::applyData(const TextureDataInfo &layoutInfo
+            , const void *memory
+            , uint32_t size
+            , Bool32 cacheMemory
+            , Bool32 createMipmaps)
+    {
+        _applyData(layoutInfo, memory, size, cacheMemory, createMipmaps);
+    }
+
+    uint32_t TextureCubeColorAttachment::getColorAttachmentWidth() const
+    {
+        return m_width;
+    }
+        
+    uint32_t TextureCubeColorAttachment::getColorAttachmentHeight() const
+    {
+        return m_height;
+    }
+    
+    uint32_t TextureCubeColorAttachment::getColorAttachmentLayers() const
+    {
+        return m_arrayLayers;
+    }
+
+    const vk::Format TextureCubeColorAttachment::getColorAttachmentFormat() const
+    {
+        return m_format;
+    }
+
+    const vk::ImageLayout TextureCubeColorAttachment::getColorAttachmentLayout() const
+    {
+        return m_layout;
+    }
+
+    const vk::ImageView *TextureCubeColorAttachment::getColorAttachmentImageView() const
+    {
+        if (m_pImageView == nullptr)throw std::runtime_error("Image view of texture don't exist!");
+        return m_pImageView->getImageView();
+    }
+
+    void TextureCubeColorAttachment::_init(Bool32 importContent)
+    {
+        Texture::_init(importContent);
+    }
+
+
+    TextureCubeDepthStencilAttachment::TextureCubeDepthStencilAttachment(vk::Format format
+        , uint32_t width
+        , uint32_t height
+        , Bool32 isInputUsage
+        , vk::ImageUsageFlags additionalUsage
+        , Bool32 defaultImageView
+        , Bool32 defaultSampler
+        )
+        : Texture(format
+        , false
+        , additionalUsage
+        , defaultImageView
+        , defaultSampler
+        )
+        , BaseDepthStencilAttachment(isInputUsage)
+    {
+        m_type = TextureType::CUBE_DEPTH_STENCIL_ATTACHMENT;
+        m_width = width;
+        m_height = height;
+        m_allAspectFlags = vk::ImageAspectFlagBits::eDepth | vk::ImageAspectFlagBits::eStencil;
+        m_usageFlags = vk::ImageUsageFlagBits::eSampled | vk::ImageUsageFlagBits::eDepthStencilAttachment;
+        m_layout = vk::ImageLayout::eDepthStencilReadOnlyOptimal;
+        if (isInputUsage)
+        {
+            m_usageFlags |= vk::ImageUsageFlagBits::eInputAttachment;
+        }
+        _checkDepthFormat();
+        _init(VG_FALSE);
+    }
+
+    TextureCubeDepthStencilAttachment::~TextureCubeDepthStencilAttachment()
+    {
+    }
+
+    uint32_t TextureCubeDepthStencilAttachment::getWidth() const
+    {
+        return m_width;
+    }
+
+    uint32_t TextureCubeDepthStencilAttachment::getHeight() const
+    {
+        return m_height;
+    }
+
+    uint32_t TextureCubeDepthStencilAttachment::getDepthStencilAttachmentWidth() const
+    {
+        return m_width;
+    }
+
+    uint32_t TextureCubeDepthStencilAttachment::getDepthStencilAttachmentHeight() const
+    {
+        return m_height;
+    }
+    
+    uint32_t TextureCubeDepthStencilAttachment::getDepthStencilAttachmentLayers() const
+    {
+        return 1u;
+    }
+
+    const vk::Format TextureCubeDepthStencilAttachment::getDepthStencilAttachmentFormat() const
+    {
+        return m_format;
+    }
+
+    const vk::ImageLayout TextureCubeDepthStencilAttachment::getDepthStencilAttachmentLayout() const
+    {
+        return m_layout;
+    }
+
+    const vk::ImageView *TextureCubeDepthStencilAttachment::getDepthStencilAttachmentImageView() const
+    {
+        return m_pImageView->getImageView();
+    }
+
+    void TextureCubeDepthStencilAttachment::applyData(const TextureDataInfo &layoutInfo
+            , const void *memory
+            , uint32_t size
+            , Bool32 cacheMemory
+            , Bool32 createMipmaps)
+    {
+        _applyData(layoutInfo, memory, size, cacheMemory, createMipmaps);
+    }
+
+    void TextureCubeDepthStencilAttachment::_init(Bool32 importContent)
+    {
+        Texture::_init(importContent);
+    }
+
+    void TextureCubeDepthStencilAttachment::_checkDepthFormat()
+    {
+        const auto &pPhysicalDevice = pApp->getPhysicalDevice();
+        const auto &props = pPhysicalDevice->getFormatProperties(m_format);
+        if ((props.optimalTilingFeatures & vk::FormatFeatureFlagBits::eDepthStencilAttachment) !=
+            vk::FormatFeatureFlagBits::eDepthStencilAttachment)
+        {
+            throw std::runtime_error("The format don't support as depth stencil attachment!");
+        }
+        if ((props.optimalTilingFeatures & vk::FormatFeatureFlagBits::eSampledImage) !=
+            vk::FormatFeatureFlagBits::eSampledImage) 
+        {
+            throw std::runtime_error("The format don't support to sample from this image!");
+        }
+    }
+
+    TextureCubeDepthAttachment::TextureCubeDepthAttachment(vk::Format format
+        , uint32_t width
+        , uint32_t height
+        , Bool32 isInputUsage
+        , vk::ImageUsageFlags additionalUsage
+        , Bool32 defaultImageView
+        , Bool32 defaultSampler
+        )
+        : Texture(format
+        , false
+        , additionalUsage
+        , defaultImageView
+        , defaultSampler
+        )
+        , BaseDepthStencilAttachment(isInputUsage)
+    {
+        m_type = TextureType::CUBE_DEPTH_ATTACHMENT;
+        m_width = width;
+        m_height = height;
+        m_allAspectFlags = vk::ImageAspectFlagBits::eDepth;
+        m_usageFlags = vk::ImageUsageFlagBits::eSampled | vk::ImageUsageFlagBits::eDepthStencilAttachment;
+        m_layout = vk::ImageLayout::eDepthStencilReadOnlyOptimal;
+        if (isInputUsage)
+        {
+            m_usageFlags |= vk::ImageUsageFlagBits::eInputAttachment;
+        }
+        _checkDepthFormat();
+        _init(VG_FALSE);
+    }
+
+    TextureCubeDepthAttachment::~TextureCubeDepthAttachment()
+    {
+    }
+
+    uint32_t TextureCubeDepthAttachment::getWidth() const
+    {
+        return m_width;
+    }
+
+    uint32_t TextureCubeDepthAttachment::getHeight() const
+    {
+        return m_height;
+    }
+
+    uint32_t TextureCubeDepthAttachment::getDepthStencilAttachmentWidth() const
+    {
+        return m_width;
+    }
+
+    uint32_t TextureCubeDepthAttachment::getDepthStencilAttachmentHeight() const
+    {
+        return m_height;
+    }
+    
+    uint32_t TextureCubeDepthAttachment::getDepthStencilAttachmentLayers() const
+    {
+        return 1u;
+    }
+
+    const vk::Format TextureCubeDepthAttachment::getDepthStencilAttachmentFormat() const
+    {
+        return m_format;
+    }
+
+    const vk::ImageLayout TextureCubeDepthAttachment::getDepthStencilAttachmentLayout() const
+    {
+        return m_layout;
+    }
+
+    const vk::ImageView *TextureCubeDepthAttachment::getDepthStencilAttachmentImageView() const
+    {
+        return m_pImageView->getImageView();
+    }
+
+    void TextureCubeDepthAttachment::applyData(const TextureDataInfo &layoutInfo
+            , const void *memory
+            , uint32_t size
+            , Bool32 cacheMemory
+            , Bool32 createMipmaps)
+    {
+        _applyData(layoutInfo, memory, size, cacheMemory, createMipmaps);
+    }
+
+    void TextureCubeDepthAttachment::_init(Bool32 importContent)
+    {
+        Texture::_init(importContent);
+    }
+
+    void TextureCubeDepthAttachment::_checkDepthFormat()
+    {
+        const auto &pPhysicalDevice = pApp->getPhysicalDevice();
+        const auto &props = pPhysicalDevice->getFormatProperties(m_format);
+        if ((props.optimalTilingFeatures & vk::FormatFeatureFlagBits::eDepthStencilAttachment) !=
+            vk::FormatFeatureFlagBits::eDepthStencilAttachment)
+        {
+            throw std::runtime_error("The format don't support as depth stencil attachment!");
+        }
+        if ((props.optimalTilingFeatures & vk::FormatFeatureFlagBits::eSampledImage) !=
+            vk::FormatFeatureFlagBits::eSampledImage) 
+        {
+            throw std::runtime_error("The format don't support to sample from this image!");
+        }
+    }
 }
--- framework/graphics/graphics/texture/texture_cube.hpp
+++ framework/graphics/graphics/texture/texture_cube.hpp
@@ -2,6 +2,7 @@
 #define VG_TEXTURE_CUBE_H
 
 #include "graphics/texture/texture.hpp"
+#include "graphics/texture/attachment.hpp"
 
 namespace vg
 {
@@ -26,6 +27,98 @@ namespace vg
             , Bool32 createMipmaps = VG_FALSE);
     private:
         TextureCube() = delete;
+    };
+
+    class TextureCubeColorAttachment : public Texture, public BaseColorAttachment
+    {
+    public:
+        TextureCubeColorAttachment(vk::Format format
+            , uint32_t width
+            , uint32_t height
+            , Bool32 isInputUsage = VG_FALSE
+            , vk::ImageUsageFlags additionalUsage = vk::ImageUsageFlags()
+            , Bool32 defaultImageView = VG_TRUE
+            , Bool32 defaultSampler = VG_TRUE
+            );
+        ~TextureCubeColorAttachment();
+        uint32_t getWidth() const;
+        uint32_t getHeight() const;
+        void applyData(const TextureDataInfo &layoutInfo
+            , const void *memory
+            , uint32_t size
+            , Bool32 cacheMemory = VG_FALSE
+            , Bool32 createMipmaps = VG_FALSE);
+        virtual uint32_t getColorAttachmentWidth() const override;
+        virtual uint32_t getColorAttachmentHeight() const override;
+        virtual uint32_t getColorAttachmentLayers() const override;
+        virtual const vk::Format getColorAttachmentFormat() const override;
+        virtual const vk::ImageLayout getColorAttachmentLayout() const override;
+        virtual const vk::ImageView *getColorAttachmentImageView() const override;
+    private:
+       TextureCubeColorAttachment() = delete;
+       virtual void _init(Bool32 importContent) override;
+    };
+
+    class TextureCubeDepthStencilAttachment : public Texture, public BaseDepthStencilAttachment
+    {
+    public:
+        TextureCubeDepthStencilAttachment(vk::Format format
+            , uint32_t width
+            , uint32_t height
+            , Bool32 isInputUsage = VG_FALSE
+            , vk::ImageUsageFlags additionalUsage = vk::ImageUsageFlags()
+            , Bool32 defaultImageView = VG_TRUE
+            , Bool32 defaultSampler = VG_TRUE
+            );
+        ~TextureCubeDepthStencilAttachment();
+        uint32_t getWidth() const;
+        uint32_t getHeight() const;
+        void applyData(const TextureDataInfo &layoutInfo
+            , const void *memory
+            , uint32_t size
+            , Bool32 cacheMemory = VG_FALSE
+            , Bool32 createMipmaps = VG_FALSE);
+        virtual uint32_t getDepthStencilAttachmentWidth() const override;
+        virtual uint32_t getDepthStencilAttachmentHeight() const override;
+        virtual uint32_t getDepthStencilAttachmentLayers() const override;
+        virtual const vk::Format getDepthStencilAttachmentFormat() const override;
+        virtual const vk::ImageLayout getDepthStencilAttachmentLayout() const override;
+        virtual const vk::ImageView *getDepthStencilAttachmentImageView() const override;
+    private:
+       TextureCubeDepthStencilAttachment() = delete;
+       virtual void _init(Bool32 importContent) override;
+       void _checkDepthFormat();
+    };
+
+    class TextureCubeDepthAttachment : public Texture, public BaseDepthStencilAttachment
+    {
+    public:
+        TextureCubeDepthAttachment(vk::Format format
+            , uint32_t width
+            , uint32_t height
+            , Bool32 isInputUsage = VG_FALSE
+            , vk::ImageUsageFlags additionalUsage = vk::ImageUsageFlags()
+            , Bool32 defaultImageView = VG_TRUE
+            , Bool32 defaultSampler = VG_TRUE
+            );
+        ~TextureCubeDepthAttachment();
+        uint32_t getWidth() const;
+        uint32_t getHeight() const;
+        void applyData(const TextureDataInfo &layoutInfo
+            , const void *memory
+            , uint32_t size
+            , Bool32 cacheMemory = VG_FALSE
+            , Bool32 createMipmaps = VG_FALSE);
+        virtual uint32_t getDepthStencilAttachmentWidth() const override;
+        virtual uint32_t getDepthStencilAttachmentHeight() const override;
+        virtual uint32_t getDepthStencilAttachmentLayers() const override;
+        virtual const vk::Format getDepthStencilAttachmentFormat() const override;
+        virtual const vk::ImageLayout getDepthStencilAttachmentLayout() const override;
+        virtual const vk::ImageView *getDepthStencilAttachmentImageView() const override;
+    private:
+       TextureCubeDepthAttachment() = delete;
+       virtual void _init(Bool32 importContent) override;
+       void _checkDepthFormat();
     };
 }
 

4c4b68355fb2a1e29d41e983a452b1e9d37d86a6
YiJiangFengYun <yijiangfengyun@outlook.com>
2018-07-27 16:26:33 +0800

    Change default materials.

--- framework/graphics/graphics/material/material_default.cpp
+++ framework/graphics/graphics/material/material_default.cpp
@@ -2,54 +2,90 @@
 
 namespace vg
 {
-    std::shared_ptr<Material> pDefaultPreDepthMaterial = nullptr;
-    std::shared_ptr<Material> pDefaultLightingDepthMaterial = nullptr;
-    std::shared_ptr<Material> pDefaultLightingPointDistMaterial = nullptr;
+    std::shared_ptr<MaterialPreDepthDefault> pDefaultPreDepthMaterial = nullptr;
+    std::shared_ptr<MaterialLightingDepthDefault> pDefaultLightingDepthMaterial = nullptr;
+    std::shared_ptr<MaterialLightingPointDistDefault> pDefaultLightingPointDistMaterial = nullptr;
 
-    class MaterialForDefault : public vg::Material
+    MaterialPreDepthDefault::MaterialPreDepthDefault()
+        : Material(VG_FALSE)
     {
-    public:
-        MaterialForDefault()
-            : vg::Material()
-        {
+        _removePass(m_pMainPass);
+        m_pMyMainShader = pDefaultPreDepthShader;
+        m_pMainShader = m_pMyMainShader.get();
+        m_pMyMainPass = pDefaultPreDepthPass;
+        m_pMainPass = m_pMyMainPass.get();
+        _addPass(m_pMainPass);
+    }
 
-        }
+    MaterialLightingDepthDefault::MaterialLightingDepthDefault()
+        : Material(VG_FALSE)
+    {
+        _removePass(m_pMainPass);
+        m_pMyMainShader = pDefaultLightingDepthShader;
+        m_pMainShader = m_pMyMainShader.get();
+        m_pMyMainPass = pDefaultLightingDepthPass;
+        m_pMainPass = m_pMyMainPass.get();
+        _addPass(m_pMainPass);
+    }
 
-        void setMainPass(Pass *pPass)
+    MaterialLightingPointDistDefault::MaterialLightingPointDistDefault()
+        : Material(VG_FALSE)
+    {
+        _removePass(m_pMainPass);
+        m_pMyMainShader = pDefaultLightingPointDistShader;
+        m_pMainShader = m_pMyMainShader.get();
+        m_pMyMainPass = pDefaultLightingPointDistPass;
+        m_pMainPass = m_pMyMainPass.get();
+        _addPass(m_pMainPass);
+    }
+
+    void MaterialLightingPointDistDefault::_beginBind(const BindInfo info, BindResult *pResult) const
+    {
+        for (uint32_t i = 0; i < 6u; ++i)
         {
-            //Remove default main pass created from class Material.
-            _removePass(m_pMainPass);
-            m_pMainShader = pPass->getShader();
-            m_pMainPass = pPass;
-            _addPass(m_pMainPass);
+            vg::RenderPassInfo trunkRenderPassInfo;
+            trunkRenderPassInfo.pRenderPass = nullptr;
+            trunkRenderPassInfo.pFramebuffer = nullptr;
+            trunkRenderPassInfo.subPassIndex = i;
+            trunkRenderPassInfo.framebufferWidth = info.framebufferWidth;
+            trunkRenderPassInfo.framebufferHeight = info.framebufferHeight;
+            trunkRenderPassInfo.projMatrix = *(info.pProjMatrix);
+            trunkRenderPassInfo.viewMatrix = *(info.pViewMatrix);
+            trunkRenderPassInfo.pPass = m_pMainPass;
+            trunkRenderPassInfo.modelMatrix = *(info.pModelMatrix);
+            trunkRenderPassInfo.pMesh = info.pMesh;
+            trunkRenderPassInfo.subMeshIndex = info.subMeshIndex;
+            trunkRenderPassInfo.viewport = fd::Viewport();
+            trunkRenderPassInfo.scissor = info.hasClipRect ? info.clipRect : fd::Rect2D();
+
+            vg::CmdInfo cmdInfo;
+            cmdInfo.pRenderPassInfo = &trunkRenderPassInfo;
+
+            pResult->pTrunkRenderPassCmdBuffer->addCmd(cmdInfo);
         }
-    private:
-    };
+    }
 
     void createDefaultMaterials()
     {
         //Pre depth material.
         {
-            pDefaultPreDepthMaterial = std::shared_ptr<Material>{
-                new MaterialForDefault()
+            pDefaultPreDepthMaterial = std::shared_ptr<MaterialPreDepthDefault>{
+                new MaterialPreDepthDefault()
             };
-            dynamic_cast<MaterialForDefault *>(pDefaultPreDepthMaterial.get())->setMainPass(pDefaultLightingDepthPass.get());
         }
 
         //Light depth material.
         {
-            pDefaultLightingDepthMaterial = std::shared_ptr<Material>{
-                new MaterialForDefault()
+            pDefaultLightingDepthMaterial = std::shared_ptr<MaterialLightingDepthDefault>{
+                new MaterialLightingDepthDefault()
             };
-            dynamic_cast<MaterialForDefault *>(pDefaultLightingDepthMaterial.get())->setMainPass(pDefaultLightingDepthPass.get());
         }
 
         //Light point distance material
         {
-            pDefaultLightingPointDistMaterial = std::shared_ptr<Material>{
-                new MaterialForDefault()
+            pDefaultLightingPointDistMaterial = std::shared_ptr<MaterialLightingPointDistDefault>{
+                new MaterialLightingPointDistDefault()
             };
-            dynamic_cast<MaterialForDefault *>(pDefaultLightingPointDistMaterial.get())->setMainPass(pDefaultLightingPointDistPass.get());
         }
         
     }
--- framework/graphics/graphics/material/material_default.hpp
+++ framework/graphics/graphics/material/material_default.hpp
@@ -6,9 +6,31 @@
 
 namespace vg
 {
-    extern std::shared_ptr<Material> pDefaultPreDepthMaterial;
-    extern std::shared_ptr<Material> pDefaultLightingDepthMaterial;
-    extern std::shared_ptr<Material> pDefaultLightingPointDistMaterial;
+    class MaterialPreDepthDefault : public vg::Material
+    {
+    public:
+        MaterialPreDepthDefault();
+    private:
+    };
+
+    class MaterialLightingDepthDefault : public vg::Material
+    {
+    public:
+        MaterialLightingDepthDefault();
+    private:
+    };
+
+    class MaterialLightingPointDistDefault : public vg::Material
+    {
+    public:
+        MaterialLightingPointDistDefault();
+    private:
+        virtual void _beginBind(const BindInfo info, BindResult *pResult) const override;
+    };
+
+    extern std::shared_ptr<MaterialPreDepthDefault> pDefaultPreDepthMaterial;
+    extern std::shared_ptr<MaterialLightingDepthDefault> pDefaultLightingDepthMaterial;
+    extern std::shared_ptr<MaterialLightingPointDistDefault> pDefaultLightingPointDistMaterial;
 
     extern void createDefaultMaterials();
     extern void destroyDefaultMaterials();
